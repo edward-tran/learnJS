b3e9b9dd860ce7c58aced9f50c9c47570aae1f28
edward <sangtranwp@gmail.com>
2025-10-13 01:06:10 +0700

    update js

--- main.js
+++ main.js
@@ -1,3 +1,101 @@
+// var manufactor = "Manufactor"
+
+// var thinkpadT470 = {
+//     name: 'ThinkPad T470',
+//     yearBuy: 2023,
+//     color: 'Black', 
+//     [manufactor]: "Lenovo"
+// }
+
+// thinkpadT470.weight = '1,4 Kg'
+// var nameKey = 'name'
+
+
+// delete thinkpadT470.Manufactor
+// console.log(thinkpadT470);
+
+// var getKeys= function (){
+//     var keysArray = []
+//     for (var i in thinkpadT470){
+//         keysArray.push(i)
+//     }
+//     return keysArray
+// }
+
+// console.log('Keys of Object: ',getKeys());
+
+// var getValues= function (){
+//     var keysValue = []
+//     for (var i in thinkpadT470){
+//         keysValue.push(thinkpadT470[i])
+//     }
+//     return keysValue
+// }
+
+// console.log('Values of Object: ', getValues());
+
+// function User(firstName, lastName){
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.getName = function (){
+//         return `${this.firstName} ${this.lastName}`
+//     }
+// }
+
+// var user1 = new User('Edward', 'Tran');
+// console.log(user1);
+// console.log('Full name: ', user1.getName());
+
+// var arr = [
+//     'Javascript', 
+//     'Python',
+//     'Go'
+// ]
+// var i = 0 
+
+// while (i < arr.length){
+//     console.log(arr[i]);
+//     i++;
+// }
+
+var courses = [
+    {
+        id: 1,
+        name: 'HTML, CSS',
+        price: 100 
+    },
+    {
+        id: 2,
+        name: 'Javascript',
+        price: 200
+    },
+    {
+        id: 3,
+        name: 'NodeJS',
+        price: 250
+    },
+]
+
+// courses.forEach((course, index)=>{
+//     console.log(course, index);
+// })
+
+
+
+// console.log(courses.filter((course, index)=>{
+//     return course.id == 2
+// }));
+
+// var updatedCourses = courses.map((course, index, originArray)=>{
+//     return {
+//         id: course.id,
+//         name: `Khoa hoc: ${course.name}`,
+//         price: course.price,
+//         coinText: `Gia: ${course.price}`,
+//         originArray:originArray
+//     }
+// })
+// console.log(updatedCourses);
 var courses = [
     {
         id: 1,
@@ -20,12 +118,12 @@ var totalPrice = courses.reduce((accumulator, currentValue)=>{
     return accumulator += currentValue.price
 },0)
 
-console.log(`Total price: ${totalPrice}`);
+// console.log(`Total price: ${totalPrice}`);
 
-// Flat: Lam phang mang 
+// Flat an array
 var depthArray = [1,2,[3,4],5,6,[7,8,9]]
 
-// Lay ra cac khoa hoc va dua vao 1 mang moi 
+// Get all courses and put into a new array 
 var topics = [
     {
         topic: 'Front-end',
@@ -54,3 +152,4 @@ var topics = [
         ]
     }
 ]
+
